524be780471c20688dfd6fb4be3e333d0a389e51
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-04-17 09:55:01 -0300

    Processamento de dados quese finalizado(falta restringir carceres especiais)

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function validarForm(){
 
     const dataNascimento = document.getElementById('dataNascimentoCliente').value.trim();
     console.log(dataNascimento);
+     
 
     const endereco = document.getElementById('enderecoCliente').value.trim();
     console.log(endereco);
@@ -42,6 +43,7 @@ function validarForm(){
 
     const dataNascimentoAnimal = document.getElementById('dataNascimentoAnimalCliente').value.trim();
     console.log(dataNascimentoAnimal);
+   
 
     const vacinaAnimal = document.getElementById('vacinaAnimalCliente').value.trim();
     console.log(vacinaAnimal);
@@ -67,6 +69,9 @@ function validarForm(){
     const brinquedoAnimal = document.getElementById('brinquedoAnimalCliente').value.trim();
     console.log(brinquedoAnimal);
 
+    const seDaBemAnimal = document.getElementById('seDaBemAnimalCliente').value.trim();
+    console.log(seDaBemAnimal);
+
     const racaoAnimal = document.getElementById('racaoAnimalCliente').value.trim();
     console.log(racaoAnimal);
 
@@ -88,6 +93,177 @@ function validarForm(){
 
     const pagamentoAnimal = document.getElementById('pagamentoAnimalCliente').value.trim();
     console.log(pagamentoAnimal);
+
+    //Processar dados
+
+    if(!nome || ){
+        alert('Preencha o nome de maneira valida.');
+        return;
+    } else{
+        console.log(nome);
+    }
+
+    if(!cpf || cpf.length !== 11 || isNaN(cpf)){
+        alert('Preencha o CPF de maneira valida.');
+        return;
+    } else{
+        console.log(cpf);
+    }
+
+    if(!idade || isNaN(idade) || idade < 18){
+        alert('Preencha a idade de maneira valida(é origatorio ser maior de idade).');
+        return;
+    } else{
+        console.log(idade);
+    }
+
+    if(!dataNascimento){
+        alert('Preencha a data de nascimento de maneira valida.');
+        return;
+    } else{
+        console.log(dataNascimento);
 }
 
+if(!endereco){
+    alert('Preencha o endereço de maneira valida.');
+    return;
+} else{
+    console.log(endereco);
+}
 
+if(!genero){
+    alert('Preencha o gênero de maneira valida.');
+    return;
+} else{
+    console.log(genero);
+}
+
+if(!contatoPessoal){
+    alert('Preencha o contato pessoal de maneira valida.');
+    return;
+} else{
+    console.log(contatoPessoal);
+}
+
+if(!sabendo){
+    alert('Preencha a parte de como ficou sabendo de nós de maneira valida.');
+    return;
+} else{
+    console.log(sabendo);
+}
+
+if(!sabendo){
+    alert('Preencha a parte de como ficou sabendo de nós de maneira valida.');
+    return;
+} else{
+    console.log(sabendo);
+}
+
+if(!jaVeio){
+    alert('Preencha a parte de se você já esteve aqui antes de maneira valida.');
+    return;
+} else{
+    console.log(jaVeio);
+}
+
+if(!tipoAnimal){
+    alert('Preencha o tipo do animal de maneira valida.');
+    return;
+} else{
+    console.log(tipoAnimal);
+}
+
+if(!racaAnimal){
+    alert('Preencha a raça do animal de maneira valida.');
+    return;
+} else{
+    console.log(racaAnimal);
+}
+
+if(!idadeAnimal || isNaN(idadeAnimal)){
+    alert('Preencha a idade do animal de maneira valida.');
+    return;
+} else{
+    console.log(idadeAnimal);
+}
+
+if(!dataNascimentoAnimal){
+    alert('Preencha a data de nascimento do animal de maneira valida.');
+    return;
+} else{
+    console.log(dataNascimentoAnimal);
+}
+
+if(!vacinaAnimal){
+    alert('Preencha a parte de vacinação do animal de maneira valida.');
+    return;
+} else{
+    console.log(vacinaAnimal);
+}
+
+if(!alergiaAnimal){
+    alert('Preencha a parte de alergia do animal de maneira valida.');
+    return;
+} else{
+    console.log(alergiaAnimal);
+}
+
+if(!pesoAnimal || isNaN(pesoAnimal)|| pesoAnimal <= 0){
+    alert('Preencha o poso do animal de maneira valida.');
+    return;
+} else{
+    console.log(pesoAnimal);
+}
+
+if(!alturaAnimal || isNaN(alturaAnimal)|| alturaAnimal <= 0){
+    alert('Preencha a altura do animal de maneira valida.');
+    return;
+} else{
+    console.log(alturaAnimal);
+}
+
+if(!motivo){
+    alert('Preencha o motivo da vinda de maneira valida.');
+    return;
+} else{
+    console.log(motivo);
+}
+
+if(!seDaBemAnimal){
+    alert('Preencha se o animal se da bem com estranhos de maneira valida.');
+    return;
+} else{
+    console.log(seDaBemAnimal);
+}
+
+if(!racaoAnimal){
+    alert('Preencha a ração do animal de maneira valida.');
+    return;
+} else{
+    console.log(racaoAnimal);
+}
+
+if(!pulgaAnimal){
+    alert('Preencha se o animal tem pulga de maneira valida.');
+    return;
+} else{
+    console.log(pulgaAnimal);
+}
+
+if(!cioAnimal){
+    alert('Preencha se o animal está no cio de maneira valida.');
+    return;
+} else{
+    console.log(cioAnimal);
+}
+
+if(!pagamentoAnimal){
+    alert('Preencha a forma de pagamento de maneira valida.');
+    return;
+} else{
+    console.log(pagamentoAnimal);
+}
+
+alert('Formulario enviado com sucesso.')
+
+}
