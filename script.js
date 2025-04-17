71612705c92183a9fb95832e135260ad21e1f649
PedroFranzoi <pedro_franzoi@estudante.sesisenai.org.br>
2025-04-17 08:25:22 -0300

    Feita a parte de pegar dados da function validarForm

---
+++ script.js
@@ -0,0 +1,93 @@
+function validarForm(){
+
+    //Pegar dados
+    const nome = document.getElementById('nomeCliente').value.trim();
+    console.log(nome);
+
+    const cpf = document.getElementById('cpfCliente').value.trim();
+    console.log(cpf);
+
+    const idade = parseInt(document.getElementById('idadeCliente').value, 10);
+    console.log(idade);
+
+    const dataNascimento = document.getElementById('dataNascimentoCliente').value.trim();
+    console.log(dataNascimento);
+
+    const endereco = document.getElementById('enderecoCliente').value.trim();
+    console.log(endereco);
+
+    const genero = document.getElementById('generoCliente').value.trim();
+    console.log(genero);
+
+    const contatoPessoal = document.getElementById('contatoPessoalCliente').value.trim();
+    console.log(contatoPessoal);
+
+    const estado = document.getElementById('estadoCliente').value.trim();
+    console.log(estado);
+
+    const sabendo = document.getElementById('sabendoCliente').value.trim();
+    console.log(sabendo);
+
+    const jaVeio = document.getElementById('jaVeioCliente').value.trim();
+    console.log(jaVeio);
+
+    const tipoAnimal = document.getElementById('tipoAnimalCliente').value.trim();
+    console.log(tipoAnimal);
+
+    const racaAnimal = document.getElementById('racaAnimalCliente').value.trim();
+    console.log(racaAnimal);
+
+    const idadeAnimal = parseInt(document.getElementById('idadeAnimalCliente').value, 10);
+    console.log(idadeAnimal);
+
+    const dataNascimentoAnimal = document.getElementById('dataNascimentoAnimalCliente').value.trim();
+    console.log(dataNascimentoAnimal);
+
+    const vacinaAnimal = document.getElementById('vacinaAnimalCliente').value.trim();
+    console.log(vacinaAnimal);
+
+    const alergiaAnimal = document.getElementById('alergiaAnimalCliente').value.trim();
+    console.log(alergiaAnimal);
+
+    const pesoAnimal = parseFloat(document.getElementById('pesoAnimalCliente').value, 10);
+    console.log(pesoAnimal);
+
+    const alturaAnimal =parseFloat(document.getElementById('alturaAnimalCliente').value, 10);
+    console.log(alturaAnimal);
+
+    const corPeloAnimal = document.getElementById('corPeloAnimalCliente').value.trim();
+    console.log(corPeloAnimal);
+
+    const corOlhoAnimal = document.getElementById('corOlhoAnimalCliente').value.trim();
+    console.log(corOlhoAnimal);
+
+    const motivo = document.getElementById('motivoAnimalCliente').value.trim();
+    console.log(motivo);
+
+    const brinquedoAnimal = document.getElementById('brinquedoAnimalCliente').value.trim();
+    console.log(brinquedoAnimal);
+
+    const racaoAnimal = document.getElementById('racaoAnimalCliente').value.trim();
+    console.log(racaoAnimal);
+
+    const linguaAnimal = document.getElementById('linguaAnimalCliente').value.trim();
+    console.log(linguaAnimal);
+
+
+    const pulgaAnimal = document.getElementById('pulgaAnimalCliente').value.trim();
+    console.log(pulgaAnimal);
+
+    const cioAnimal = document.getElementById('cioAnimalCliente').value.trim();
+    console.log(cioAnimal);
+
+    const passeioAnimal = document.getElementById('passeioAnimalCliente').value.trim();
+    console.log(passeioAnimal);
+
+    const quemAmaAnimal = document.getElementById('quemAmaAnimalCliente').value.trim();
+    console.log(quemAmaAnimal);
+
+    const pagamentoAnimal = document.getElementById('pagamentoAnimalCliente').value.trim();
+    console.log(pagamentoAnimal);
+}
+
+
